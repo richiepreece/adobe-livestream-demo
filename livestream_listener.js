bb414875489554b1cf5fb2cac02d4356e36c4ec9
Richie Preece <preece@adobe.com>
2018-03-29 09:31:22 -0600

    Update livestream_listener.js

--- livestream_listener.js
+++ livestream_listener.js
@@ -3,6 +3,16 @@ const _ = require('lodash');
 
 // Total hits
 var hits = 0;
+var recentHits = 0;
+var uniqueVisitors = {};
+var events = {};
+var props = {};
+var eVars = {};
+var desks = {};
+var texts = [];
+var eventData = [];
+
+var stopAfter = 1000;
 
 connector.on('hit', function (hit) {
     // Increment total hit counter.
@@ -18,4 +28,176 @@ connector.on('hit', function (hit) {
 
 connector.on('writeToDB', function () {
 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connector.on('deskHit', function (desk) {
+    desks[desk] = desks[desk] || 0;
+    desks[desk] = desks[desk] + 1;
 });
